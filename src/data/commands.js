a8dde4d8eb7a6702384a411ba36350dc43d9463e
Olof Jondelius <olof.jondelius@hemnet.se>
2019-08-13 17:02:47 +0200

    Full list of commands

--- src/data/commands.js
+++ src/data/commands.js
@@ -1,18 +1,128 @@
 const commands ={
-    take: {
-        url: "https://guides.rubyonrails.org/active_record_querying.html#take",
+    avarage: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#average",
+    },
+    count: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#calculations",
+    },
+    //Should be exists?
+    exists: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#existence-of-objects",
+    },
+    explain: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#running-explain",
     },
     find: {
         url: "https://guides.rubyonrails.org/active_record_querying.html#find",
     },
+    find_by: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#find-by",
+    },
+    find_by_sql: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#finding-by-sql",
+    },
+    find_each: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#find-each",
+    },
+    find_in_batches: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#find-in-batches", 
+    },
+    //Should also exist with ! - https://guides.rubyonrails.org/active_record_querying.html#find-or-create-by-bang
+    find_or_create_by: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#find-or-create-by",
+    },
+    find_or_initialize_by: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#find-or-initialize-by"
+    },
     first: {
         url: "https://guides.rubyonrails.org/active_record_querying.html#first",
+    },
+    group: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#group",
+    },
+    having: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#having",
+    },
+    includes: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations",
+    },
+    ids: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#ids",
+    },
+    joins: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#joining-tables",
     },
     last: {
         url: "https://guides.rubyonrails.org/active_record_querying.html#last",
     },
-    find_by: {
-        url: "https://guides.rubyonrails.org/active_record_querying.html#find-by",
+    left_outer_join: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#left-outer-joins",
+    },
+    limit: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#limit-and-offset",
+    },
+    lock: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#locking-records-for-update",
+    },
+    maximum: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#maximum",
+    },
+    minimum: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#minimum",
+    },
+    none: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#null-relation",
+    },
+    not: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#not-conditions",
+    },
+    offset: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#limit-and-offset",
+    },
+    only: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#only",
+    },
+    or: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#or-conditions",
+    },
+    order: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#ordering",
+    },
+    pluck: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#pluck",
+    },
+    readonly: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#readonly-objects",
+    },
+    reorder: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#reorder",
+    },
+    reverse_order: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#reverse-order",
+    },
+    rewhere: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#rewhere",
+    },
+    select: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#selecting-specific-fields",
+    },
+    select_all: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#select-all",
+    },
+    sum: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#sum",
+    },
+    take: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#take",
+    },
+    unscope: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#unscope",
+    },
+    unscoped: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#removing-all-scoping",
+    },
+    where: {
+        url: "https://guides.rubyonrails.org/active_record_querying.html#conditions",
     },
 }
 
